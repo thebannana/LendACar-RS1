fad19f5e690795cf002e6cd45740ea65e573515c
Emin Branković <emin.brankovic19@gmail.com>
2024-11-05 22:18:25 +0100

    init

--- backend/LendACarAPI/Data/Models/CompanyPosition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LendACarAPI.Data.Models
-{
-    public class CompanyPosition
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
