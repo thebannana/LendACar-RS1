e1f43ff6e7b1a8b65b9cda27222274520ff2c66e
Emin Branković <emin.brankovic19@gmail.com>
2024-11-05 22:21:06 +0100

    Changing the sln file location and adding test endpoint to check if everything still works

---
+++ backend/LendACarAPI/Data/Models/VehicleReview.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LendACarAPI.Data.Models
+{
+    public class VehicleReview
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Reviewer))]
+        [Required]
+        public int ReviewerId { get; set; }
+        public User? Reviewer { get; set; }
+
+        [ForeignKey(nameof(Vehicle))]
+        [Required]
+        public int VehicleID { get; set; }
+        public Vehicle? Vehicle { get; set; }
+        [Required]
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
