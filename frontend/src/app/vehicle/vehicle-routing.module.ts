c669a22011ce12caf1917c0927ed12ea97df815c
Edin Tabak <etabak61@gmail.com>
2024-11-13 19:21:32 +0100

    Route do vehicle-category form

--- frontend/src/app/vehicle/vehicle-routing.module.ts
+++ frontend/src/app/vehicle/vehicle-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import { CategoryFormComponent } from './vehicle-category/vehicle-category.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
       {path: '**', redirectTo: 'home', pathMatch: 'full'}
     ]
   },
+  {path:'vehicle-category',component: CategoryFormComponent},
 ];
 
 
