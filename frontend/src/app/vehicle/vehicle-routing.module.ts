af271878f0f03892204353cd0145371dd0c6ad71
Edin Tabak <etabak61@gmail.com>
2024-11-15 10:42:08 +0100

    Update vehicle-categories routing

--- frontend/src/app/vehicle/vehicle-routing.module.ts
+++ frontend/src/app/vehicle/vehicle-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import { CategoryFormComponent } from './vehicle-category/vehicle-category.component';
+import { VehicleCategoriesComponent } from './vehicle-category/vehicle-category.component';
 
 const routes: Routes = [
   {
@@ -10,7 +10,7 @@ const routes: Routes = [
       {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
       {path:'dashboard', component:DashboardComponent},
       {path: '**', redirectTo: 'home', pathMatch: 'full'},
-      {path:'vehicle-category',component: CategoryFormComponent}
+      {path:'vehicle-category',component: VehicleCategoriesComponent}
     ]
   }
 ];
